dc60e820f23015f2809a536f5b68c525ffd6008f
Sven Fuchs <me@svenfuchs.com>
2017-05-03 17:52:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-exec > >(ruby f.rb) 2>&1
+# exec > >(ruby filter.rb) 2>&1
+exec > >(ruby pty.rb ruby filter.rb) 2>&1
 
-echo "tty?"
 tty
 
-for i in $(seq 1 500); do
+for i in $(seq 1 100); do
   printf .
   sleep 0.01
 done
